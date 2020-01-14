f7a159e18105971ccb9b8783c1d8d0b11852dc6b
mmbajo <markarjaybajo@gmail.com>
2020-01-14 21:48:35 +0900

    add test case

--- FourSumUnitTests.py
+++ FourSumUnitTests.py
@@ -1,2 +1,16 @@
-import FourSum
+from FourSum import fourSum
 import unittest
+
+
+def sortAndStringify(array):
+    return ','.join(sorted(list(map(lambda x: str(x), array))))
+
+
+class TestFourSum(unittest.TestCase):
+    def test_case1(self):
+        output = fourSum([1, 2, 3, 4, 5, 6, 7], 10)
+        output = list(map(sortAndStringify, output))
+        quads = [[1, 2, 3, 4]]
+        self.assertTrue(len(output) == 1)
+        for quad in quads:
+            self.assertTrue(sortAndStringify(quad) in output)
