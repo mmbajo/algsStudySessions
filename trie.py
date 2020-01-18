1904af8936fc6d513bd12e13a273ea9a44808073
mmbajo <markarjaybajo@gmail.com>
2020-01-18 17:17:47 +0900

    Create Trie skeleton

---
+++ trie.py
@@ -0,0 +1,13 @@
+
+
+class SuffixTrie:
+    def __init__(self, string):
+        self.root = {}
+        self.endSymbol = '*'
+        self.populateSufficTrieFrom(string)
+
+    def populateSufficTrieFrom(self, string):
+        pass
+
+    def contains(self, string):
+        pass
---
+++ trieUnitTests.py
@@ -0,0 +1,7 @@
+import trie
+import unittest
+
+
+class TestTrieImplementation(unittest.TestCase):
+    def test_case1(self):
+        pass
