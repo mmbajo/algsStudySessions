8cd4a0a9a3cf273cde8db40008b772c8dcfd3137
mmbajo <markarjaybajo@gmail.com>
2020-01-18 17:48:59 +0900

    Trie Implementation attempt 1

--- trie.py
+++ trie.py
@@ -4,10 +4,23 @@ class SuffixTrie:
     def __init__(self, string):
         self.root = {}
         self.endSymbol = '*'
-        self.populateSufficTrieFrom(string)
+        self.populateSuffixTrieFrom(string)
 
-    def populateSufficTrieFrom(self, string):
-        pass
+    def populateSuffixTrieFrom(self, string):
+        for i in range(len(string)):
+            node = self.root
+            for j in range(i, len(string)):
+                letter = string[j]
+                if letter not in node:
+                    node[letter] = {}
+                node = node[letter]
+            node[self.endSymbol] = True
 
     def contains(self, string):
-        pass
+        currNode = self.root
+        for j in range(len(string)):
+            currLetter = string[j]
+            if currLetter not in currNode:
+                return False
+            currNode = currNode[currLetter]
+        return self.endSymbol in currNode
--- trieUnitTests.py
+++ trieUnitTests.py
@@ -5,3 +5,7 @@ import unittest
 class TestTrieImplementation(unittest.TestCase):
     def test_case1(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
