c1f3e3f214cfe568c337881a1b4d61eef94ecd41
mmbajo <markarjaybajo@gmail.com>
2020-01-26 15:50:01 +0900

    add another solution

--- sameBsts.py
+++ sameBsts.py
@@ -1,37 +1,93 @@
-# O(n^2) time | O(n^2) space
-# Where n is equal to the length of array
 from typing import List
 
 
-def sameBsts(arrayOne: List[float], arrayTwo: List[float]) -> bool:
-    if len(arrayOne) == 0 and len(arrayTwo) == 0:
-        return True
+class Solution1:
+    '''
+    O(n^2) time | O(n^2) space
+    Where n is equal to the length of array
+    '''
 
-    if len(arrayOne) != len(arrayTwo):
-        return False
+    def sameBsts(self, arrayOne: List[float], arrayTwo: List[float]) -> bool:
+        if len(arrayOne) == 0 and len(arrayTwo) == 0:
+            return True
 
-    if arrayOne[0] != arrayTwo[0]:
-        return False
+        if len(arrayOne) != len(arrayTwo):
+            return False
 
-    leftBstOne = getLeftBst(arrayOne)
-    rightBstOne = getRightBst(arrayOne)
-    leftBstTwo = getLeftBst(arrayTwo)
-    rightBstTwo = getRightBst(arrayTwo)
+        if arrayOne[0] != arrayTwo[0]:
+            return False
 
-    return sameBsts(leftBstOne, leftBstTwo) and sameBsts(rightBstOne, rightBstTwo)
+        leftBstOne = self.getLeftBst(arrayOne)
+        rightBstOne = self.getRightBst(arrayOne)
+        leftBstTwo = self.getLeftBst(arrayTwo)
+        rightBstTwo = self.getRightBst(arrayTwo)
+
+        return (self.sameBsts(leftBstOne, leftBstTwo) and
+                self.sameBsts(rightBstOne, rightBstTwo))
+
+    def getLeftBst(self, array: List[float]):
+        cache = []
+        for i in range(1, len(array)):
+            if array[i] < array[0]:
+                cache.append(array[i])
+        return cache
+
+    def getRightBst(self, array: List[float]):
+        cache = []
+        for i in range(1, len(array)):
+            if array[0] <= array[i]:
+                cache.append(array[i])
+        return cache
 
 
-def getLeftBst(array: List[float]):
-    cache = []
-    for i in range(1, len(array)):
-        if array[i] < array[0]:
-            cache.append(array[i])
-    return cache
+class Solution2:
+    '''
+    O(n^2) time | O(d^2) space
+    where n is the length of array and d is depth of tree
+    '''
 
+    def sameBsts(self, arrayOne: List[float], arrayTwo: List[float]) -> bool:
+        return self.areSameBsts(arrayOne,
+                                arrayTwo,
+                                0,
+                                0,
+                                float("-inf"),
+                                float("inf"))
 
-def getRightBst(array: List[float]):
-    cache = []
-    for i in range(1, len(array)):
-        if array[0] <= array[i]:
-            cache.append(array[i])
-    return cache
+    def areSameBsts(self, arrayOne: List[float], arrayTwo: List[float],
+                    rootIdxOne: int, rootIdxTwo: int,
+                    minVal: float, maxVal: float) -> bool:
+        if rootIdxOne == -1 or rootIdxTwo == -1:
+            return rootIdxOne == rootIdxTwo
+
+        if arrayOne[rootIdxOne] != arrayTwo[rootIdxTwo]:
+            return False
+
+        leftRootIdxOne = self.getSmallerIdx(arrayOne, rootIdxOne, minVal)
+        rightRootIdxOne = self.getBiggerOrEqualIdx(arrayOne, rootIdxOne, maxVal)
+        leftRootIdxTwo = self.getSmallerIdx(arrayTwo, rootIdxTwo, minVal)
+        rightRootIdxTwo = self.getBiggerOrEqualIdx(arrayTwo, rootIdxTwo, maxVal)
+
+        # Recursion
+        currentVal = arrayOne[rootIdxOne]
+        isLeftBst = self.areSameBsts(arrayOne, arrayTwo,
+                                     leftRootIdxOne, leftRootIdxTwo,
+                                     minVal, currentVal)
+        isRightBst = self.areSameBsts(arrayOne, arrayTwo,
+                                      rightRootIdxOne, rightRootIdxTwo,
+                                      currentVal, maxVal)
+        return isLeftBst and isRightBst
+
+    def getSmallerIdx(self, array: List[float], startIdx: int,
+                      minVal: float) -> int:
+        for i in range(startIdx + 1, len(array)):
+            if minVal <= array[i] < array[startIdx]:
+                return i
+        return -1
+
+    def getBiggerOrEqualIdx(self, array: List[float], startIdx: int,
+                            maxVal: float) -> int:
+        for i in range(startIdx + 1, len(array)):
+            if array[startIdx] <= array[i] < maxVal:
+                return i
+        return -1
--- sameBstsUnitTests.py
+++ sameBstsUnitTests.py
@@ -1,11 +1,18 @@
 import unittest
-import sameBsts as sb
+from sameBsts import Solution1, Solution2
 
 
 class TestMe(unittest.TestCase):
     def test_case1(self):
         arrayOne = [2, 3, 4, 5, 6, 7, 8]
         arrayTwo = [2, 3, 4, 5, 6, 7, 8]
+        sb = Solution1()
+        self.assertEqual(sb.sameBsts(arrayOne, arrayTwo), True)
+
+    def test_case2(self):
+        arrayOne = [2, 3, 4, 5, 6, 7, 8]
+        arrayTwo = [2, 3, 4, 5, 6, 7, 8]
+        sb = Solution2()
         self.assertEqual(sb.sameBsts(arrayOne, arrayTwo), True)
 
 
