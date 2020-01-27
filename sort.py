9cd38d9a08983d76660cf61ebab9eec4d501c9d4
mmbajo <markarjaybajo@gmail.com>
2020-01-27 21:11:31 +0900

    Program quicksort!!

--- sort.py
+++ sort.py
@@ -9,6 +9,7 @@ class HeapSort:
     '''
 
     def sort(self, array: List[float]) -> List[float]:
+        print("---------------------HeapSort--------------------------------")
         self.buildMaxHeap(array)
         for endIdx in reversed(range(1, len(array))):
             self.swap(0, endIdx, array)
@@ -49,4 +50,40 @@ class QuickSort:
     Worst case: O(n^2) time | O(1) space
     '''
 
-    def sort(self, )
+    def sort(self, array: List[float]) -> List[float]:
+        print("---------------------QuickSort--------------------------------")
+        self.quickSortHelper(0, len(array) - 1, array)
+        return array
+
+    def quickSortHelper(self, startIdx: int, endIdx: int, array: List[float]):
+        if startIdx >= endIdx:
+            return
+        leftIdx, rightIdx = self.threeWayPartition(startIdx, endIdx, array)
+        self.quickSortHelper(startIdx, leftIdx - 1, array)
+        self.quickSortHelper(rightIdx + 1, endIdx, array)
+
+    def threeWayPartition(self, startIdx: int, endIdx: int, array: List[float]):
+        partitioner = array[startIdx]
+        lessIdx = startIdx
+        equalIdx = startIdx
+        greaterIdx = endIdx
+
+        while equalIdx <= greaterIdx:
+            if array[equalIdx] == partitioner:
+                equalIdx += 1
+                print('{} Increment equalIdx -> {}'.format(array, equalIdx))
+            elif array[equalIdx] < partitioner:
+                self.swap(equalIdx, lessIdx, array)
+                equalIdx += 1
+                lessIdx += 1
+                print('{} Swap vals equalIdx and lessIdx! Increment equalIdx -> {} and lessIdx -> {}'
+                      .format(array, equalIdx, lessIdx))
+            elif array[equalIdx] > partitioner:
+                self.swap(equalIdx, greaterIdx, array)
+                greaterIdx -= 1
+                print('{} Swap vals equalIdx and greaterIdx! Decrement greaterIdx -> {}'
+                      .format(array, greaterIdx))
+        return lessIdx, greaterIdx
+
+    def swap(self, i: int, j: int, array: List[float]):
+        array[i], array[j] = array[j], array[i]
--- sortUnitTests.py
+++ sortUnitTests.py
@@ -1,5 +1,5 @@
 import unittest
-from sort import HeapSort
+from sort import HeapSort, QuickSort
 
 
 class TestSortingAlgorithms(unittest.TestCase):
@@ -7,6 +7,11 @@ class TestSortingAlgorithms(unittest.TestCase):
         array = [12, 3, 4, 5, 2, 1, 4, 5]
         out = HeapSort().sort(array)
         self.assertEqual(sorted(array), out)
+
+    def test_QuickSort(self):
+        array = [0, 0, 0, 0, 0, -1, -36, 45, 34, 243, 0, 0, 0]
+        out = QuickSort().sort(array)
+        self.assertEqual(out, sorted(array))
 
 
 if __name__ == '__main__':
