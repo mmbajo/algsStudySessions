6bfdab36aab9612594d49965f04f602db50a1bef
mmbajo <markarjaybajo@gmail.com>
2020-02-01 00:03:32 +0900

    Whew! Completed the boggle board solution

--- boggleBoard.py
+++ boggleBoard.py
@@ -1,8 +1,65 @@
-from typing import List
+from typing import List, Dict
 
 
 class BoggleBoardSolution:
-    pass
+    def boggleBoard(self,
+                    boggle: List[List[str]],
+                    words: List[str]) -> List[str]:
+        # Add words on a Trie
+        trie = Trie()
+        for word in words:
+            trie.add(word)
+        # Initialize visited 2D Boolean array
+        visited = [[False for letter in row] for row in boggle]
+        # Initialize Cache for found Words
+        inBoggleBoard = {}
+        # Explore every BoggleBoard element
+        for i in range(len(boggle)):
+            for j in range(len(boggle[i])):
+                self.explore(i, j, boggle, trie.root, visited, inBoggleBoard)
+        return list(inBoggleBoard.keys())
+
+    def explore(self, i: int, j: int,
+                boggle: List[List[str]],
+                trieNode: Dict,
+                visited: List[List[bool]],
+                inBoggleBoard: Dict):
+        if visited[i][j]:
+            return
+        currLetter = boggle[i][j]
+        if currLetter not in trieNode:
+            return
+        visited[i][j] = True
+        trieNode = trieNode[currLetter]
+        if '*' in trieNode:
+            inBoggleBoard[trieNode['*']] = True
+        neighbors = self.getNeighbors(i, j, boggle)
+        for neighbor in neighbors:
+            self.explore(neighbor[0], neighbor[1],
+                         boggle, trieNode, visited, inBoggleBoard)
+        visited[i][j] = False
+
+    def getNeighbors(self, i: int, j: int, boggle: List[List[str]]) -> List[List[int]]:
+        maxColIdx = len(boggle[0]) - 1
+        maxRowIdx = len(boggle) - 1
+        neighbors = []
+        if i < maxRowIdx and j < maxColIdx:
+            neighbors.append([i + 1, j + 1])
+        if i < maxRowIdx and j > 0:
+            neighbors.append([i + 1, j - 1])
+        if i > 0 and j < maxColIdx:
+            neighbors.append([i - 1, j + 1])
+        if i > 0 and j > 0:
+            neighbors.append([i - 1, j - 1])
+        if i > 0:
+            neighbors.append([i - 1, j])
+        if j > 0:
+            neighbors.append([i, j - 1])
+        if i < maxRowIdx:
+            neighbors.append([i + 1, j])
+        if j < maxColIdx:
+            neighbors.append([i, j + 1])
+        return neighbors
 
 
 class Trie:
--- boggleBoardUnitTests.py
+++ boggleBoardUnitTests.py
@@ -1,4 +1,4 @@
-from boggleBoard import Trie
+from boggleBoard import Trie, BoggleBoardSolution
 import unittest
 
 
@@ -10,6 +10,21 @@ class TestImplementation(unittest.TestCase):
         for word in listOfWords:
             trie.add(word)
         self.assertEqual(expected, trie.root)
+
+    def test_boggleBoardSolution(self):
+        boggleRowWords = ['thisisa', 'simplex', 'bxxxxeb', 'xogglxo',
+                          'xxxDTra', 'REPEAdx', 'xxxxxxx', 'NOTRE-P',
+                          'xxDETAE']
+        words = ['this', 'is', 'not', 'a', 'simple', 'boggle',
+                 'board', 'test', 'REPEATED', 'NOTRE-PEATED']
+        expected = ['this', 'is', 'a', 'simple', 'boggle', 'board',
+                    'NOTRE-PEATED']
+        boggle = [list(wordRow) for wordRow in boggleRowWords]
+        solution = BoggleBoardSolution()
+        out = solution.boggleBoard(boggle, words)
+        self.assertEqual(len(out), len(expected))
+        for word in out:
+            self.assertTrue(word in expected)
 
 
 if __name__ == '__main__':
